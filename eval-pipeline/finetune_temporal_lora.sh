9b7de9f80a62465e8a36892b0d34c0e71d3b38a3
serkanakin <sserkan.akin08@gmail.com>
2025-06-16 17:05:37 +0200

    feat: add attention implementation and device map options in finetune_temporal_lora.sh

--- eval-pipeline/finetune_temporal_lora.sh
+++ eval-pipeline/finetune_temporal_lora.sh
@@ -56,6 +56,7 @@ done
 # ------------------------------- Training -----------------------------------
 python3 -m llava.train.train_mem \
   --model_name_or_path            "$MODEL" \
+  --attn_implementation           torch \
   --version                       plain \
   --data_path                     "$DATA" \
   --image_folder                  "$IMG_ROOT" \
@@ -74,4 +75,5 @@ python3 -m llava.train.train_mem \
   --logging_steps                 20 \
   --bf16                          true \
   --lazy_preprocess               true \
+  --device_map                    "auto" \
   --output_dir                    "$OUT"
